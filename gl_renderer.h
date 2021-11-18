df75d9f33cfa4c67057c62e1bd1b5d15f0302efa
Tuncay Yavuz Büyükadalı <76534892+Yavuz1234567890@users.noreply.github.com>
2021-11-18 23:49:57 +0300

    better renderer

--- gl_renderer.h
+++ gl_renderer.h
@@ -15,8 +15,8 @@ namespace renderer
 		gl_renderer() = default;
 		virtual ~gl_renderer() = default;
 
-		void create_context();
-		void draw_mesh(gl_mesh* mesh, const draw_mode& mode = draw_mode::ARRAY);
-		void draw_mesh_with_shader(gl_mesh* mesh, gl_shader* shader, const draw_mode& mode = draw_mode::ARRAY);
+		static void create_context();
+		static void draw_mesh(gl_mesh* mesh, const draw_mode& mode = draw_mode::ARRAY);
+		static void draw_mesh_with_shader(gl_mesh* mesh, gl_shader* shader, const draw_mode& mode = draw_mode::ARRAY);
 	};
 }
